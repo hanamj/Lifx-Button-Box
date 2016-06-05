7db025dee62aeab353ae5e17f20ae3860d65a5da
John Hanam <jhanam@blackberry.com>
2016-06-04 20:14:57 -0400

    .

--- butt.js
+++ butt.js
@@ -135,7 +135,7 @@ function updateFirebase() {
 function changeLight() {
   var c = calculateColor();
   if ((c.r == 0) && (c.g == 0) && (c.b == 0)) {
-    b = 0.1
+    b = 0.05
   } else {
     b = BRIGHTNESS
   }
